0ba7733f4c2bfc37e387f866dcff6dbf2e3086b0
hmz007 <hmz007@gmail.com>
2018-05-16 18:05:18 +0800

    configs: rk33plat: enable BOOTDELAY (1)
    Change-Id: Ia227d19f9def2b0677501ac71bb622e0ab7b42f8
    Signed-off-by: hmz007 <hmz007@gmail.com>

--- include/configs/rk33plat.h
+++ include/configs/rk33plat.h
@@ -199,7 +199,7 @@
 #endif /* CONFIG_SECUREBOOT_CRYPTO */
 
 /* mod it to enable console commands.	*/
-#define CONFIG_BOOTDELAY		0
+#define CONFIG_BOOTDELAY		1
 
 /* efuse version */
 #ifdef CONFIG_RK_EFUSE
