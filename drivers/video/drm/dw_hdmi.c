hardkernel/u-boot

commit 1f71919f4fef703c4ab1881e06539b1533267634

drm/rockchip: dw-hdmi: support ddc i2c bus
Signed-off-by: Shunqing Chen <csq@rock-chips.com>
Change-Id: Iec5fc4ea5c5c13069a8fe0895222028d1a739edc

drivers/video/drm/dw_hdmi.c
@@ -11,6 +11,7 @@
 #include <asm/arch/vendor.h>
 #include <edid.h>
 #include <dm/device.h>
+#include <dm/of_access.h>
 #include <dm/ofnode.h>
 #include <dm/read.h>
 #include <linux/hdmi.h>
@@ -2270,6 +2271,7 @@ int rockchip_dw_hdmi_init(struct display_state *state)
 	struct drm_display_mode *mode_buf;
 	ofnode hdmi_node = conn_state->node;
 	u32 val;
+	struct device_node *ddc_node;
 
 	hdmi = malloc(sizeof(struct dw_hdmi));
 	if (!hdmi)
@@ -2292,6 +2294,14 @@ int rockchip_dw_hdmi_init(struct display_state *state)
 		hdmi->hdcp1x_enable = true;
 	else
 		hdmi->hdcp1x_enable = false;
+
+	ddc_node = of_parse_phandle(ofnode_to_np(hdmi_node), "ddc-i2c-bus", 0);
+	if (ddc_node) {
+		uclass_get_device_by_ofnode(UCLASS_I2C, np_to_ofnode(ddc_node),
+					    &hdmi->adap.i2c_bus);
+		if (hdmi->adap.i2c_bus)
+			hdmi->adap.ops = i2c_get_ops(hdmi->adap.i2c_bus);
+	}
 
 	hdmi->grf = syscon_get_first_range(ROCKCHIP_SYSCON_GRF);
 	if (hdmi->grf <= 0) {

friendlyarm/uboot-rockchip

commit dbc5962857b8f9ffa21ae9acb7f728420924e856

drm/rockchip: dw-hdmi: support for standard i2c
Fixes: 1f71919f4fe ("drm/rockchip: dw-hdmi: support ddc i2c bus")
Signed-off-by: hmz007 <hmz007@gmail.com>

drivers/video/drm/dw_hdmi.c
@@ -512,6 +512,14 @@ static int dw_hdmi_i2c_xfer(struct ddc_adapter *adap,
 	u8 addr = msgs[0].addr;
 	int i, ret = 0;
 
+	if (adap->ops) {
+		ret = adap->ops->xfer(adap->i2c_bus, msgs, num);
+		if (!ret)
+			ret = num;
+
+		return ret;
+	}
+
 	printf("xfer: num: %d, addr: %#x\n", num, addr);
 	for (i = 0; i < num; i++) {
 		if (msgs[i].len == 0) {
@@ -2329,6 +2337,8 @@ int rockchip_dw_hdmi_init(struct display_state *state)
 			hdmi->adap.ops = i2c_get_ops(hdmi->adap.i2c_bus);
 	}
 
+	hdmi->adap.ddc_xfer = dw_hdmi_i2c_xfer;
+
 	hdmi->grf = syscon_get_first_range(ROCKCHIP_SYSCON_GRF);
 	if (hdmi->grf <= 0) {
 		printf("%s: Get syscon grf failed (ret=%p)\n",
@@ -2353,7 +2363,6 @@ int rockchip_dw_hdmi_init(struct display_state *state)
 		hdmi->i2c = malloc(sizeof(struct dw_hdmi_i2c));
 		if (!hdmi->i2c)
 			return -ENOMEM;
-		hdmi->adap.ddc_xfer = dw_hdmi_i2c_xfer;
 
 		/*
 		 * Read high and low time from device tree. If not available use