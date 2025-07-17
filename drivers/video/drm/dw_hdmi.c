LubanCat/u-boot

commit 9c170041dcf97ed3fe5bbd76e70cda2ed3fcbd92

edid: Support hdmi2.1 extend block
Support getting HF-EEODB and HF-SCDB from edid.

Change-Id: Ib1bfe76d1e58c1346e4a0a6105a623a7ac8c8a3a
Signed-off-by: Algea Cao <algea.cao@rock-chips.com>

drivers/video/drm/dw_hdmi.c
@@ -2632,7 +2632,7 @@ static void rockchip_dw_hdmi_mode_valid(struct dw_hdmi *hdmi)
 
 int rockchip_dw_hdmi_get_timing(struct rockchip_connector *conn, struct display_state *state)
 {
-	int ret, i, vic;
+	int ret = 0, i, vic;
 	struct connector_state *conn_state = &state->conn_state;
 	struct dw_hdmi *hdmi = conn->data;
 	struct edid *edid = (struct edid *)conn_state->edid;
@@ -2641,9 +2641,9 @@ int rockchip_dw_hdmi_get_timing(struct rockchip_connector *conn, struct display_
 	if (!hdmi)
 		return -EFAULT;
 
-	ret = drm_do_get_edid(&hdmi->adap, conn_state->edid);
+	conn_state->edid = drm_do_get_edid(&hdmi->adap);
 
-	if (!ret) {
+	if (conn_state->edid) {
 		hdmi->sink_has_audio = drm_detect_monitor_audio(edid);
 		if (hdmi->sink_has_audio)
 			hdmi->sink_is_hdmi = true;
@@ -2652,7 +2652,7 @@ int rockchip_dw_hdmi_get_timing(struct rockchip_connector *conn, struct display_
 
 		ret = drm_add_edid_modes(&hdmi->edid_data, conn_state->edid);
 	}
-	if (ret < 0) {
+	if (ret <= 0) {
 		hdmi->sink_is_hdmi = true;
 		hdmi->sink_has_audio = true;
 		do_cea_modes(&hdmi->edid_data, def_modes_vic,
@@ -2711,11 +2711,13 @@ int rockchip_dw_hdmi_detect(struct rockchip_connector *conn, struct display_stat
 
 int rockchip_dw_hdmi_get_edid(struct rockchip_connector *conn, struct display_state *state)
 {
-	int ret;
+	int ret = 0;
 	struct connector_state *conn_state = &state->conn_state;
 	struct dw_hdmi *hdmi = conn->data;
 
-	ret = drm_do_get_edid(&hdmi->adap, conn_state->edid);
+	conn_state->edid = drm_do_get_edid(&hdmi->adap);
+	if (!conn_state->edid)
+		ret = -EINVAL;
 
 	return ret;
 }

drivers/video/drm/inno_hdmi.c
@@ -739,7 +739,7 @@ static int rockchip_inno_hdmi_detect(struct rockchip_connector *conn, struct dis
 static int rockchip_inno_hdmi_get_timing(struct rockchip_connector *conn,
 					 struct display_state *state)
 {
-	int  i, ret;
+	int  i, ret = 0;
 	struct connector_state *conn_state = &state->conn_state;
 	struct drm_display_mode *mode = &conn_state->mode;
 	struct inno_hdmi *hdmi = conn->data;
@@ -749,8 +749,8 @@ static int rockchip_inno_hdmi_get_timing(struct rockchip_connector *conn,
 	if (!hdmi)
 		return -EFAULT;
 
-	ret = drm_do_get_edid(&hdmi->adap, conn_state->edid);
-	if (!ret) {
+	conn_state->edid = drm_do_get_edid(&hdmi->adap);
+	if (conn_state->edid) {
 		hdmi->hdmi_data.sink_is_hdmi =
 			drm_detect_hdmi_monitor(edid);
 		hdmi->hdmi_data.sink_has_audio = drm_detect_monitor_audio(edid);

common/edid.c
@@ -3333,25 +3333,101 @@ static void drm_parse_hdmi_deep_color_info(struct hdmi_edid_data *data,
 }
 
 /*
+ * References:
+ * - CTA-861-H section 7.3.3 CTA Extension Version 3
+ */
+static int cea_db_collection_size(const u8 *cta)
+{
+	u8 d = cta[2];
+
+	if (d < 4 || d > 127)
+		return 0;
+
+	return d - 4;
+}
+
+#define CTA_EXT_DB_HF_EEODB		0x78
+#define CTA_DB_EXTENDED_TAG		7
+
+static int cea_db_tag(const u8 *db);
+static int cea_db_payload_len(const u8 *db);
+static int cea_db_extended_tag(const u8 *db);
+
+static bool cea_db_is_extended_tag(const void *db, int tag)
+{
+	return cea_db_tag(db) == CTA_DB_EXTENDED_TAG &&
+		cea_db_payload_len(db) >= 1 &&
+		cea_db_extended_tag(db) == tag;
+}
+
+static bool cea_db_is_hdmi_forum_eeodb(const void *db)
+{
+	return cea_db_is_extended_tag(db, CTA_EXT_DB_HF_EEODB) &&
+		cea_db_payload_len(db) >= 2;
+}
+
+static int edid_hfeeodb_extension_block_count(const struct edid *edid)
+{
+	const u8 *cta;
+
+	/* No extensions according to base block, no HF-EEODB. */
+	if (!edid->extensions)
+		return 0;
+
+	/* HF-EEODB is always in the first EDID extension block only */
+	cta = (u8 *)edid + HDMI_EDID_BLOCK_SIZE * 1;
+	if (cta[0] != CEA_EXT || cta[1] < 3)
+		return 0;
+
+	/* Need to have the data block collection, and at least 3 bytes. */
+	if (cea_db_collection_size(cta) < 3)
+		return 0;
+
+	/*
+	 * Sinks that include the HF-EEODB in their E-EDID shall include one and
+	 * only one instance of the HF-EEODB in the E-EDID, occupying bytes 4
+	 * through 6 of Block 1 of the E-EDID.
+	 */
+	if (!cea_db_is_hdmi_forum_eeodb(&cta[4]))
+		return 0;
+
+	return cta[4 + 2];
+}
+
+static int edid_hfeeodb_block_count(const struct edid *edid)
+{
+	int eeodb = edid_hfeeodb_extension_block_count(edid);
+
+	return eeodb ? eeodb + 1 : 0;
+}
+
+/*
  * Search EDID for CEA extension block.
  */
-static u8 *drm_find_edid_extension(struct edid *edid, int ext_id)
+static u8 *drm_find_edid_extension(const struct edid *edid,
+				   int ext_id)
 {
 	u8 *edid_ext = NULL;
 	int i;
+	int len;
 
 	/* No EDID or EDID extensions */
-	if (!edid || !edid->extensions)
+	if (edid == NULL || edid->extensions == 0)
 		return NULL;
 
+	if (edid_hfeeodb_extension_block_count(edid))
+		len = edid_hfeeodb_extension_block_count(edid);
+	else
+		len = edid->extensions;
+
 	/* Find CEA extension */
-	for (i = 0; i < edid->extensions; i++) {
-		edid_ext = (u8 *)edid + EDID_SIZE * (i + 1);
+	for (i = 0; i < len; i++) {
+		edid_ext = (u8 *)edid + HDMI_EDID_BLOCK_SIZE * (i + 1);
 		if (edid_ext[0] == ext_id)
 			break;
 	}
 
-	if (i == edid->extensions)
+	if (i >= len)
 		return NULL;
 
 	return edid_ext;
@@ -3462,6 +3538,24 @@ drm_parse_hdmi_vsdb_video(struct hdmi_edid_data *data, const u8 *db)
 	drm_parse_hdmi_deep_color_info(data, db);
 }
 
+#define USE_EXTENDED_TAG 0x07
+#define CTA_EXT_DB_HF_SCDB 0x000079
+
+static bool cea_db_is_scdb(const u8 *db)
+{
+	unsigned int oui;
+
+	if (cea_db_tag(db) != USE_EXTENDED_TAG)
+		return false;
+
+	if (cea_db_payload_len(db) < 7)
+		return false;
+
+	oui = db[3] << 16 | db[2] << 8 | db[1];
+
+	return oui == CTA_EXT_DB_HF_SCDB;
+}
+
 static void drm_parse_cea_ext(struct hdmi_edid_data *data,
 			      struct edid *edid)
 {
@@ -3490,7 +3584,7 @@ static void drm_parse_cea_ext(struct hdmi_edid_data *data,
 
 		if (cea_db_is_hdmi_vsdb(db))
 			drm_parse_hdmi_vsdb_video(data, db);
-		if (cea_db_is_hdmi_forum_vsdb(db))
+		if (cea_db_is_hdmi_forum_vsdb(db) || cea_db_is_scdb(db))
 			drm_parse_hdmi_forum_vsdb(data, db);
 		if (cea_db_is_y420cmdb(db))
 			drm_parse_y420cmdb_bitmap(data, db);
@@ -3574,49 +3668,92 @@ static void drm_add_display_info(struct hdmi_edid_data *data, struct edid *edid)
 		info->color_formats |= DRM_COLOR_FORMAT_YCRCB422;
 }
 
+/*
+ * Search EDID for CEA extension block.
+ */
+static u8 *drm_find_edid_extension_from_index(const struct edid *edid,
+					      int ext_id, int *ext_index)
+{
+	u8 *edid_ext = NULL;
+	int i;
+	int len;
+
+	/* No EDID or EDID extensions */
+	if (edid == NULL || edid->extensions == 0)
+		return NULL;
+
+	if (edid_hfeeodb_extension_block_count(edid))
+		len = edid_hfeeodb_extension_block_count(edid);
+	else
+		len = edid->extensions;
+
+	/* Find CEA extension */
+	for (i = *ext_index; i < len; i++) {
+		edid_ext = (u8 *)edid + HDMI_EDID_BLOCK_SIZE * (i + 1);
+		if (edid_ext[0] == ext_id)
+			break;
+	}
+
+	if (i >= len)
+		return NULL;
+
+	*ext_index = i + 1;
+
+	return edid_ext;
+}
+
 static
 int add_cea_modes(struct hdmi_edid_data *data, struct edid *edid)
 {
-	const u8 *cea = drm_find_cea_extension(edid);
+	const u8 *cea;
 	const u8 *db, *hdmi = NULL, *video = NULL;
 	u8 dbl, hdmi_len, video_len = 0;
-	int modes = 0;
+	int i, count = 0, modes = 0;
+	int ext_index = 0;
 
-	if (cea && cea_revision(cea) >= 3) {
-		int i, start, end;
+	if (edid_hfeeodb_extension_block_count(edid))
+		count = edid_hfeeodb_extension_block_count(edid);
+	else
+		count = edid->extensions;
 
-		if (cea_db_offsets(cea, &start, &end))
-			return 0;
+	for (i = 0; i < count; i++) {
+		ext_index = i;
+		cea = drm_find_edid_extension_from_index(edid, CEA_EXT, &ext_index);
+		if (cea && cea_revision(cea) >= 3) {
+			int i, start, end;
 
-		for_each_cea_db(cea, i, start, end) {
-			db = &cea[i];
-			dbl = cea_db_payload_len(db);
+			if (cea_db_offsets(cea, &start, &end))
+				return 0;
 
-			if (cea_db_tag(db) == EDID_CEA861_DB_VIDEO) {
-				video = db + 1;
-				video_len = dbl;
-				modes += do_cea_modes(data, video, dbl);
-			} else if (cea_db_is_hdmi_vsdb(db)) {
-				hdmi = db;
-				hdmi_len = dbl;
-			} else if (cea_db_is_y420vdb(db)) {
-				const u8 *vdb420 = &db[2];
+			for_each_cea_db(cea, i, start, end) {
+				db = &cea[i];
+				dbl = cea_db_payload_len(db);
 
-				/* Add 4:2:0(only) modes present in EDID */
-				modes += do_y420vdb_modes(data, vdb420,
-							  dbl - 1);
+				if (cea_db_tag(db) == EDID_CEA861_DB_VIDEO) {
+					video = db + 1;
+					video_len = dbl;
+					modes += do_cea_modes(data, video, dbl);
+				} else if (cea_db_is_hdmi_vsdb(db)) {
+					hdmi = db;
+					hdmi_len = dbl;
+				} else if (cea_db_is_y420vdb(db)) {
+					const u8 *vdb420 = &db[2];
+
+					/* Add 4:2:0(only) modes present in EDID */
+					modes += do_y420vdb_modes(data, vdb420,
+								dbl - 1);
+				}
 			}
 		}
+
+		/*
+		* We parse the HDMI VSDB after having added the cea modes as we will
+		* be patching their flags when the sink supports stereo 3D.
+		*/
+		if (hdmi)
+			modes += do_hdmi_vsdb_modes(hdmi, hdmi_len, video,
+						video_len, data);
 	}
-
-	/*
-	 * We parse the HDMI VSDB after having added the cea modes as we will
-	 * be patching their flags when the sink supports stereo 3D.
-	 */
-	if (hdmi)
-		modes += do_hdmi_vsdb_modes(hdmi, hdmi_len, video,
-					    video_len, data);
-
 	return modes;
 }
 
@@ -6864,13 +7001,18 @@ drm_do_probe_ddc_edid(struct ddc_adapter *adap, u8 *buf, unsigned int block,
 	return ret == xfers ? 0 : -1;
 }
 
-int drm_do_get_edid(struct ddc_adapter *adap, u8 *edid)
+u8 *drm_do_get_edid(struct ddc_adapter *adap)
 {
-	int i, j, block_num, block = 0;
+	int i, j, block_num, valid_extensions = 0, invalid_blocks = 0, block = 0;
 	bool edid_corrupt;
+	u8 *new, *edid;
 #ifdef DEBUG
 	u8 *buff;
 #endif
+
+	edid = malloc(HDMI_EDID_BLOCK_SIZE);
+	if (!edid)
+		goto err;
 
 	/* base block fetch */
 	for (i = 0; i < 4; i++) {
@@ -6888,28 +7030,84 @@ int drm_do_get_edid(struct ddc_adapter *adap, u8 *edid)
 	if (i == 4)
 		goto err;
 
-	block++;
-	/* get the number of extensions */
-	block_num = edid[0x7e];
+	/* if there's no extensions, we're done */
+	valid_extensions = edid[0x7e];
+	if (valid_extensions == 0)
+		return 0;
 
-	for (j = 1; j <= block_num; j++) {
+	new = realloc(edid, (valid_extensions + 1) * HDMI_EDID_BLOCK_SIZE);
+	if (!new)
+		goto err;
+	edid = new;
+
+	/* get the number of extensions */
+	block_num = edid[0x7e] + 1;
+
+	for (j = 1; j < block_num; j++) {
+		u8 *block = edid + j * HDMI_EDID_BLOCK_SIZE;
+
 		for (i = 0; i < 4; i++) {
-			if (drm_do_probe_ddc_edid(adap, &edid[0x80 * j], j,
+			if (drm_do_probe_ddc_edid(adap, block, j,
 						  HDMI_EDID_BLOCK_SIZE))
 				goto err;
-			if (drm_edid_block_valid(&edid[0x80 * j], j,
+			if (drm_edid_block_valid(block, j,
 						 true, NULL))
 				break;
 		}
 
 		if (i == 4)
+			invalid_blocks++;
+
+		if (j == 1) {
+			/*
+			 * If the first EDID extension is a CTA extension, and
+			 * the first Data Block is HF-EEODB, override the
+			 * extension block count.
+			 *
+			 * Note: HF-EEODB could specify a smaller extension
+			 * count too, but we can't risk allocating a smaller
+			 * amount.
+			 */
+			int eeodb = edid_hfeeodb_block_count((const struct edid *)edid);
+
+			if (eeodb > block_num) {
+				block_num = eeodb;
+				new = realloc(edid, block_num * HDMI_EDID_BLOCK_SIZE);
+				if (!new)
+					goto err;
+				edid = new;
+			}
+		}
+	}
+
+	if (invalid_blocks) {
+		u8 *base;
+
+		new = kcalloc(valid_extensions + 1, HDMI_EDID_BLOCK_SIZE, GFP_KERNEL);
+		if (!new)
 			goto err;
-		block++;
+
+		base = new;
+		for (i = 0; i <= edid[0x7e]; i++) {
+			u8 *block = edid + i * HDMI_EDID_BLOCK_SIZE;
+
+			if (!drm_edid_block_valid(block, i, false, NULL))
+				continue;
+
+			memcpy(base, block, HDMI_EDID_BLOCK_SIZE);
+			base += HDMI_EDID_BLOCK_SIZE;
+		}
+
+		new[HDMI_EDID_BLOCK_SIZE - 1] += new[0x7e] - valid_extensions;
+		new[0x7e] = valid_extensions;
+
+		kfree(edid);
+		edid = new;
 	}
 
 #ifdef DEBUG
 	printf("RAW EDID:\n");
-	for (i = 0; i < block_num + 1; i++) {
+	for (i = 0; i < block_num; i++) {
 		buff = &edid[0x80 * i];
 		for (j = 0; j < HDMI_EDID_BLOCK_SIZE; j++) {
 			if (j % 16 == 0)
@@ -6920,13 +7118,12 @@ int drm_do_get_edid(struct ddc_adapter *adap, u8 *edid)
 	}
 #endif
 
-	return 0;
+	return edid;
 
 err:
 	printf("can't get edid block:%d\n", block);
-	/* clear all read edid block, include invalid block */
-	memset(edid, 0, HDMI_EDID_BLOCK_SIZE * (block + 1));
-	return -EFAULT;
+
+	return NULL;
 }
 
 static ssize_t hdmi_ddc_read(struct ddc_adapter *adap, u16 addr, u8 offset,

drivers/video/drm/analogix_dp.c
@@ -921,9 +921,11 @@ static int analogix_dp_connector_get_edid(struct rockchip_connector *conn,
 {
 	struct connector_state *conn_state = &state->conn_state;
 	struct analogix_dp_device *dp = dev_get_priv(conn->dev);
-	int ret;
+	int ret = 0;
 
-	ret = drm_do_get_edid(&dp->aux.ddc, conn_state->edid);
+	conn_state->edid = drm_do_get_edid(&dp->aux.ddc);
+	if (!conn_state->edid)
+		ret = -EINVAL;
 
 	return ret;
 }
@@ -1222,7 +1224,7 @@ static int analogix_dp_connector_get_timing(struct rockchip_connector *conn,
 	struct vop_rect rect;
 	u32 yuv_fmts_mask = DRM_COLOR_FORMAT_YCRCB444 | DRM_COLOR_FORMAT_YCRCB422;
 	u32 fmt_id;
-	int ret, i;
+	int ret = 0, i;
 
 	mode_buf = malloc(MODE_LEN * sizeof(struct drm_display_mode));
 	if (!mode_buf)
@@ -1232,11 +1234,11 @@ static int analogix_dp_connector_get_timing(struct rockchip_connector *conn,
 	memset(&edid_data, 0, sizeof(struct hdmi_edid_data));
 	edid_data.mode_buf = mode_buf;
 
-	ret = drm_do_get_edid(&dp->aux.ddc, conn_state->edid);
-	if (!ret)
+	conn_state->edid = drm_do_get_edid(&dp->aux.ddc);
+	if (conn_state->edid)
 		ret = drm_add_edid_modes(&edid_data, conn_state->edid);
 
-	if (ret < 0) {
+	if (ret <= 0) {
 		printf("failed to get edid\n");
 		goto err;
 	}

drivers/video/drm/dw-dp.c
@@ -1459,7 +1459,9 @@ static int dw_dp_connector_get_edid(struct rockchip_connector *conn, struct disp
 	struct connector_state *conn_state = &state->conn_state;
 	struct dw_dp *dp = connector_to_dw_dp(conn);
 
-	ret = drm_do_get_edid(&dp->aux.ddc, conn_state->edid);
+	conn_state->edid = drm_do_get_edid(&dp->aux.ddc);
+	if (!conn_state->edid)
+		ret = -EINVAL;
 
 	return ret;
 }
@@ -1626,7 +1628,7 @@ static u32 dw_dp_get_output_bus_fmts(struct dw_dp *dp, struct hdmi_edid_data *ed
 
 static int dw_dp_connector_get_timing(struct rockchip_connector *conn, struct display_state *state)
 {
-	int ret, i;
+	int ret = 0, i;
 	struct connector_state *conn_state = &state->conn_state;
 	struct dw_dp *dp = connector_to_dw_dp(conn);
 	struct drm_display_mode *mode = &conn_state->mode;
@@ -1644,11 +1646,11 @@ static int dw_dp_connector_get_timing(struct rockchip_connector *conn, struct di
 	edid_data.mode_buf = mode_buf;
 
 	if (!dp->force_output) {
-		ret = drm_do_get_edid(&dp->aux.ddc, conn_state->edid);
-		if (!ret)
+		conn_state->edid = drm_do_get_edid(&dp->aux.ddc);
+		if (conn_state->edid)
 			ret = drm_add_edid_modes(&edid_data, conn_state->edid);
 
-		if (ret < 0) {
+		if (ret <= 0) {
 			printf("failed to get edid\n");
 			goto err;
 		}

drivers/video/drm/dw_hdmi_qp.c
@@ -345,8 +345,12 @@ static int dw_hdmi_i2c_read(struct dw_hdmi_qp *hdmi,
 			}
 
 			i = 20;
-			hdmi_modb(hdmi, I2CM_FM_READ, I2CM_WR_MASK,
-				  I2CM_INTERFACE_CONTROL0);
+			if (i2c->is_segment)
+				hdmi_modb(hdmi, I2CM_EXT_READ, I2CM_WR_MASK,
+					  I2CM_INTERFACE_CONTROL0);
+			else
+				hdmi_modb(hdmi, I2CM_FM_READ, I2CM_WR_MASK,
+					  I2CM_INTERFACE_CONTROL0);
 
 			while (i--) {
 				udelay(1000);
@@ -531,7 +535,7 @@ static int dw_hdmi_i2c_xfer(struct ddc_adapter *adap,
 			i2c->is_segment = true;
 			hdmi_modb(hdmi, DDC_SEGMENT_ADDR, I2CM_SEG_ADDR,
 				  I2CM_INTERFACE_CONTROL1);
-			hdmi_modb(hdmi, *msgs[i].buf, I2CM_SEG_PTR,
+			hdmi_modb(hdmi, *msgs[i].buf << 7, I2CM_SEG_PTR,
 				  I2CM_INTERFACE_CONTROL1);
 		} else {
 			if (msgs[i].flags & I2C_M_RD)
@@ -1452,24 +1456,25 @@ static void rockchip_dw_hdmi_qp_mode_valid(struct dw_hdmi_qp *hdmi)
 }
 
 static int _rockchip_dw_hdmi_qp_get_timing(struct rockchip_connector *conn,
-					   struct display_state *state, int edid_status)
+					   struct display_state *state)
 {
 	int i;
 	struct connector_state *conn_state = &state->conn_state;
 	struct dw_hdmi_qp *hdmi = conn->data;
 	struct edid *edid = (struct edid *)conn_state->edid;
 	const u8 def_modes_vic[6] = {4, 16, 2, 17, 31, 19};
+	int ret = 0;
 
 	if (!hdmi)
 		return -EFAULT;
 
-	if (!edid_status) {
+	if (edid) {
 		hdmi->sink_is_hdmi =
 			drm_detect_hdmi_monitor(edid);
 		hdmi->sink_has_audio = drm_detect_monitor_audio(edid);
-		edid_status = drm_add_edid_modes(&hdmi->edid_data, conn_state->edid);
+		ret = drm_add_edid_modes(&hdmi->edid_data, conn_state->edid);
 	}
-	if (edid_status < 0) {
+	if (ret <= 0) {
 		hdmi->sink_is_hdmi = true;
 		hdmi->sink_has_audio = true;
 		do_cea_modes(&hdmi->edid_data, def_modes_vic,
@@ -1501,14 +1506,13 @@ int rockchip_dw_hdmi_qp_get_timing(struct rockchip_connector *conn, struct displ
 {
 	struct connector_state *conn_state = &state->conn_state;
 	struct dw_hdmi_qp *hdmi = conn->data;
-	int ret;
 
-	ret = drm_do_get_edid(&hdmi->adap, conn_state->edid);
+	conn_state->edid = drm_do_get_edid(&hdmi->adap);
 
 	if (conn_state->secondary)
-		_rockchip_dw_hdmi_qp_get_timing(conn_state->secondary, state, ret);
+		_rockchip_dw_hdmi_qp_get_timing(conn_state->secondary, state);
 
-	return _rockchip_dw_hdmi_qp_get_timing(conn, state, ret);
+	return _rockchip_dw_hdmi_qp_get_timing(conn, state);
 }
 
 
@@ -1527,11 +1531,13 @@ int rockchip_dw_hdmi_qp_detect(struct rockchip_connector *conn, struct display_s
 
 int rockchip_dw_hdmi_qp_get_edid(struct rockchip_connector *conn, struct display_state *state)
 {
-	int ret;
+	int ret = 0;
 	struct connector_state *conn_state = &state->conn_state;
 	struct dw_hdmi_qp *hdmi = conn->data;
 
-	ret = drm_do_get_edid(&hdmi->adap, conn_state->edid);
+	conn_state->edid = drm_do_get_edid(&hdmi->adap);
+	if (!conn_state->edid)
+		ret = -EINVAL;
 
 	return ret;
 }

drivers/video/drm/rockchip_display.h
@@ -235,7 +235,7 @@ struct connector_state {
 
 	struct drm_display_mode mode;
 	struct overscan overscan;
-	u8 edid[EDID_SIZE * 4];
+	u8 *edid;
 	int bus_format;
 	u32 bus_flags;
 	int output_mode;

drivers/video/drm/sii902x.c
@@ -727,13 +727,13 @@ static int sii902x_get_timing(struct udevice *dev)
 	struct display_state *state = bridge->state;
 	struct connector_state *conn_state = &state->conn_state;
 	struct drm_display_mode *mode = &conn_state->mode;
-	int ret;
+	int ret = 0;
 
-	ret = drm_do_get_edid(&sii902x->adap, conn_state->edid);
-	if (!ret) {
+	conn_state->edid = drm_do_get_edid(&sii902x->adap);
+	if (conn_state->edid)
 		ret = drm_add_edid_modes(&sii902x->edid_data, conn_state->edid);
-	}
-	if (ret < 0) {
+
+	if (ret <= 0) {
 		dev_err(dev, "Failed to get edid %d\n", ret);
 		return ret;
 	}

friendlyarm/uboot-rockchip

commit 44f011c4ba9b543cf5e29305ab22c2b2e75f5d10

drm/rockchip: hdmi: fixes using reinitialized edid
Fixes: 9c170041dcf ("edid: Support hdmi2.1 extend block")
Signed-off-by: Jensen Huang <jensenhuang@friendlyarm.com>

drivers/video/drm/dw_hdmi.c
@@ -2660,15 +2660,14 @@ int rockchip_dw_hdmi_get_timing(struct rockchip_connector *conn, struct display_
 	int ret = 0, i, vic;
 	struct connector_state *conn_state = &state->conn_state;
 	struct dw_hdmi *hdmi = conn->data;
-	struct edid *edid = (struct edid *)conn_state->edid;
 	const u8 def_modes_vic[6] = {4, 16, 2, 17, 31, 19};
 
 	if (!hdmi)
 		return -EFAULT;
 
 	conn_state->edid = drm_do_get_edid(&hdmi->adap);
-
 	if (conn_state->edid) {
+		struct edid *edid = (struct edid *) conn_state->edid;
 		hdmi->sink_has_audio = drm_detect_monitor_audio(edid);
 		if (hdmi->sink_has_audio)
 			hdmi->sink_is_hdmi = true;

drivers/video/drm/inno_hdmi.c
@@ -743,7 +743,6 @@ static int rockchip_inno_hdmi_get_timing(struct rockchip_connector *conn,
 	struct connector_state *conn_state = &state->conn_state;
 	struct drm_display_mode *mode = &conn_state->mode;
 	struct inno_hdmi *hdmi = conn->data;
-	struct edid *edid = (struct edid *)conn_state->edid;
 	const u8 def_modes_vic[6] = {16, 4, 2, 17, 31, 19};
 
 	if (!hdmi)
@@ -751,6 +750,7 @@ static int rockchip_inno_hdmi_get_timing(struct rockchip_connector *conn,
 
 	conn_state->edid = drm_do_get_edid(&hdmi->adap);
 	if (conn_state->edid) {
+		struct edid *edid = (struct edid *)conn_state->edid;
 		hdmi->hdmi_data.sink_is_hdmi =
 			drm_detect_hdmi_monitor(edid);
 		hdmi->hdmi_data.sink_has_audio = drm_detect_monitor_audio(edid);